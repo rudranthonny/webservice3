3ec6db2732504d6bf28f327978134e333d6d431f
Anthonny <ing.anthonny.joel@gmail.com>
2022-06-20 13:18:38 -0500

    mod

--- resources/views/users/login_moodle.blade.php
+++ resources/views/users/login_moodle.blade.php
@@ -6,16 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <script>
-    window.onload=function(){
+    /*window.onload=function(){
                 // Una vez cargada la página, el formulario se enviara automáticamente.
 		document.forms["login_moodle"].submit();
-    }
+    }*/
     </script>
 </head>
 <body>
     <form action="http://aprendiendo.jademlearning.com/login/index.php" id='login_moodle' name="login_moodle" class="mt-2" method="post">
                 <input type="hidden" name="username" value="{{Auth::user()->dni}}">
                 <input type="hidden" name="password" value="{{$contrasena}}">
-    </form>
+                <button type="submit">Ingresa con...</button>
+            </form>
 </body>
 </html>
